346fe2514d5fee1916b824c896825123e9df0275
David Arques <david.darqsa@gmail.com>
2022-08-19 22:39:36 +0200

    Added user card style and skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import UserCard from "./components/UserCard/UserCard";
+
 function App() {
-  return <h1>🤣🤣👌😎</h1>;
+  return (
+    <>
+      <h1>🤣🤣👌😎</h1>
+      <UserCard />
+    </>
+  );
 }
 
 export default App;
