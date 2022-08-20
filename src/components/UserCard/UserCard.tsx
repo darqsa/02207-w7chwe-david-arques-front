b2b5e4c4ae633f7915d074692c536ff7915d6839
David Arques <david.darqsa@gmail.com>
2022-08-20 11:18:17 +0200

    Updated User card style (buttons on hover)

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -3,12 +3,18 @@ import { UserContainerStyled } from "./UserCardStyled";
 const UserCard = (): JSX.Element => {
   return (
     <UserContainerStyled className="user-container">
-      <img
-        className="user-container__image"
-        src="https://navigatedlearning.org/assets/gooru/profile-d8cfa58723f9c1daaa7ff6986738f816.png"
-        alt="profile"
-        width={150}
-      />
+      <div className="user-container__image-container user-container__image-container--friend">
+        <img
+          className="user-container__image"
+          src="https://t3.ftcdn.net/jpg/03/53/11/00/360_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg"
+          alt="profile"
+          height={160}
+        />
+        <div className="user-container__button-container">
+          <button className="user-container__button">Unfriend</button>
+          <button className="user-container__button">Make enemy</button>
+        </div>
+      </div>
       <span className="user-container__name">@DavidPutoAmo</span>
     </UserContainerStyled>
   );
--- src/components/UserCard/UserCardStyled.ts
+++ src/components/UserCard/UserCardStyled.ts
@@ -8,21 +8,54 @@ const UserContainerStyled = styled.div`
   gap: 20px;
   background-color: #a5c9ca;
   padding: 30px 20px;
-  border-radius: 50px;
+  border-radius: 20px;
   margin: 20px;
   .user-container {
-    &__image {
+    &__image-container {
+      border-radius: 50%;
+      overflow: hidden;
+      width: 150px;
+      height: 150px;
+      border: 6px solid grey;
+      position: relative;
+
+      &--friend {
+        border: 6px solid green;
+      }
+    }
+    &__button-container {
+      display: flex;
+      flex-direction: column;
+      width: 150px;
+      height: 150px;
+      align-items: center;
+      justify-content: center;
+      z-index: 1;
+      position: absolute;
+      top: 0px;
+      border-radius: 50%;
+      opacity: 0;
+      background-color: #2c333370;
+      transition: 0.15s;
+      &:hover {
+        opacity: 100;
+      }
+    }
+    &__button {
       cursor: pointer;
+    }
+    &__image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
     &__name {
       font-weight: bold;
-      color: #2c3333;
-      text-shadow: 1px 2px 2px #00000010;
+      color: #3a3b3c;
       transition: 0.1s;
       &:hover {
         color: #e7f6f2;
         cursor: pointer;
-        text-shadow: 1px 2px 2px #00000050;
       }
     }
   }
